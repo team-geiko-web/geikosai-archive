feac07ecad112afd628a1292b2b43f75802aae1e
後藤尋 <100586810+hir05o1@users.noreply.github.com>
2024-06-18 20:20:50 +0900

    rm all files

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
-}
--- pages/index.vue
+++
@@ -1,1 +0,0 @@
-<template><NuxtWelcome /></template>
